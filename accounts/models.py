6d48021900016ae61be6cde05a2c45ecacfe91e2
amirmojarad <amirmojarad13@gmail.com>
2022-04-22 16:57:43 +0430

    user model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class GenderChoices(models.Choices):
+    Female = "F"
+    Male = "M"
+    
 
 
 class User(AbstractUser):
-        
+    address = models.TextField(blank=True, null=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GenderChoices.choices, null=True, blank=True)
+    
